d1f92365d2827ee5a00f009a538a9399d0abe328
Clinton Rono <c36clinton@gmail.com>
2022-07-06 00:53:34 +0300

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-''' class Student that defines a student
-'''
+"""method for student creation"""
 
 
 class Student:
-    '''module class student
-    '''
+    """Student obj, interesting how you don't have to directly
+    test for strings in a loop, python is weird"""
 
     def __init__(self, first_name, last_name, age):
-        '''method __init__
-        '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        new_dictionary = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                new_dictionary[key] = value
+        return new_dictionary
