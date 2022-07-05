45e31ec6d1dcf94edcb627f0eb44db5e079d7c97
Clinton Rono <c36clinton@gmail.com>
2022-07-05 16:58:09 +0300

    Update 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-'''function tthat returns the JSON rep of an object (string)
+''' function that returns the JSON representation of an object (string)
 '''
 
 import json
 
-def to_json_string(my_obj):
-    '''module to_json_string returnd JSON represenation
-    '''
 
+def to_json_string(my_obj):
+    ''' module to_json_strin
+     returns JSON representation
+    '''
     return json.dumps(my_obj)
