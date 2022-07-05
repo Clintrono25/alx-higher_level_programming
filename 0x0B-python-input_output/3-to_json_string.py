90ed1b30abaeb2aee44ea17e32bde462eee7d074
Clinton Rono <Clinton.Rono@amref.org>
2022-07-05 16:55:41 +0300

    Created 3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function tthat returns the JSON rep of an object (string)
+"""
+
+import json
+
+def to_json_string(my_obj):
+    '''module to_json_string returnd JSON represenation
+    '''
+
+    return json.dumps(my_obj)
