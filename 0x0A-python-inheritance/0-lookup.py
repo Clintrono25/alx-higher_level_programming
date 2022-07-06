14337347268b57c9c3d7253ffc6b2a3d86c57c14
Clinton Rono <Clinton.Rono@amref.org>
2022-07-06 16:33:33 +0300

    Created 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''function that returns the list of availabale attributes and methods of an object
+'''
+
+def lookup(obj):
+    '''function: lookup() Returns a list object'''
+    return dir(obj)
